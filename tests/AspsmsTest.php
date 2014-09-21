ecfdab21f5bcccff817aede60f14ca658b9e93c2
Nadar <basilsuter@Basils-MacBook-Pro.local>
2014-09-21 16:00:24 +0200

    phpunit test case

--- tests/AspsmsTest.php
+++ tests/AspsmsTest.php
@@ -7,10 +7,39 @@ use Aspsms\Aspsms;
 
 class AspsmsTest extends PHPUnit_Framework_TestCase {
 	
-	public function testEquals() {
-		$aspsms = new Aspsms(ASPSMS_KEY, ASPSMS_PASSWORD, array("Originator" => ASPSMS_ORIGINATOR));
+	public $aspsms = null;
+	
+	public function setUp () {
+		$this->aspsms = new Aspsms(ASPSMS_KEY, ASPSMS_PASSWORD, array("Originator" => ASPSMS_ORIGINATOR));
+	}
+	
+	public function testSendTextSms() {
 		
-		var_dump(count($aspsms));
+		$sendSms = $this->aspsms->sendTextSms("example", array("number"));
+		
+		$this->assertEquals(FALSE, $sendSms);
+	}
+	
+	public function testVerifyTrackingNumber () {
+		$value = "123456789abcedefghijklmnopqrstuv";
+		
+		$this->assertEquals($value, $this->aspsms->verifyTrackingNumber($value));
+		
+		$value = "+123456789!?abcdefg";
+		$this->assertEquals("123456789", $this->aspsms->verifyMobileNumber($value));
+	}
+	
+	public function testVerifyMobileNumber () {
+		$value = "0123456789";
+		$this->assertEquals($value, $this->aspsms->verifyMobileNumber($value));
+		
+		$value = "+0123456789abcdefgh!?";
+		$this->assertEquals("0123456789", $this->aspsms->verifyMobileNumber($value));
+	}
+	
+	public function testDeliveryStatus () {
+		$tr = $this->aspsms->deliveryStatus("12345");
+		var_dump($tr);
 	}
 	
 }
